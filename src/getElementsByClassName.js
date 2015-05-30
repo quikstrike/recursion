5211c307d3e59abfbf28e66a2b21431d870df96c
quikstrike <quikstrik3@gmail.com>
2015-05-30 17:45:54 -0500

    Recursion Assignment Completed

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,24 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
+  var elementList = [];
+  var currentElement = arguments[1] || document.body;
+  var elementsClasses = currentElement.classList;
+
+  if(elementsClasses !== undefined){
+
+    if(elementsClasses.contains(className)){
+      elementList.push(currentElement);
+    }
+
+    if(currentElement.childNodes.length > 0){
+      for (var key in currentElement.childNodes){
+        elementList = elementList.concat(getElementsByClassName(className,currentElement.childNodes[key]));
+      }
+    }
+    
+  }
+  return elementList;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,47 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null){
+    return "null";
+  }
+  if (typeof obj === 'number' || typeof obj === 'boolean'){
+    return obj.toString();
+  }
+  if (typeof obj === 'string'){
+    return '"'+obj+'"';
+  }
+
+  if (Array.isArray(obj)){
+    if (obj.length === 0){
+      return '[]';
+    }else{
+      var newArr = [];
+      for(var i = 0;i < obj.length ; i++){
+        newArr.push(stringifyJSON(obj[i]));
+      }
+      return '[' + newArr.join(",") + ']';
+    }
+  }
+
+  if(typeof obj === 'object'){
+      var length = 0;
+      var jsonArray = [];
+      for (var key in obj){
+        length++;
+      }
+      if (length === 0){
+        return '{}';
+      } else {
+        for (var key in obj){
+          if(obj[key] === undefined || typeof obj[key] === 'function'||typeof key === 'function'){
+            jsonArray.push(null);
+          } else {
+            jsonArray.push(stringifyJSON(key) + ':' + stringifyJSON(obj[key]));
+          }
+        }
+          //Here We are returning the array of items, but we are filtering the null's out.
+          return '{'+jsonArray.filter(function(val){return val !== null;}).join(",")+'}'
+      }
+  }
+
 };
